5e7f32a8192dbe182c32a2dae822a2dc3beab69e
Eric Fiorani <efiorani305@gmail.com>
2018-08-23 15:17:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,8 @@ class App < Sinatra::Base
     @power_forward = params[:pf]
     @small_forward = params[:sf]
     @center = params[:c]
-
+    "#{@team_name} #{@coach} #{@team_name} #{@team_name}
+    #{@team_name} #{@team_name} #{@team_name} #{@team_name}"
     erb :team
   end
 end
